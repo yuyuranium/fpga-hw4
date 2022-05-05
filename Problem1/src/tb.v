aefad061b85953904f77e51a08d4f4e8423b257c
japoka410666 <1026jason@gmail.com>
2022-05-05 20:37:18 +0800

    modify tb.v

--- Problem1/src/tb.v
+++ Problem1/src/tb.v
@@ -35,25 +35,30 @@ module tb();
     
     initial begin
     //Initailize
-    clk=1'b0; rst=1'b0; ENA=1'b0; ENB=1'b0; WEA=4'b0; WEB=4'b0;
-    ADDRA=10'b0; ADDRB=10'b0; DINA=32'b0; DINB=32'b0;
+    clk=1'b0; rst=1'b1; ENA=1'b0; ENB=1'b0; WEA=4'b0000; WEB=4'b0000;
+    ADDRA=10'd0; ADDRB=10'd0; DINA=32'd0; DINB=32'd0;
     #10
-    rst = 1'b1;
+    rst = 1'b0;
     #10
     
     //Test READ 
-    rst = 1'b0;
+    rst = 1'b1;
+    ENA=1'b1; ENB=1'b1;
+    
     ADDRA=10'd0; ADDRB=10'd4;
     #100
     ADDRA=10'd28; ADDRB=10'd64;
     #100
     
-    //Test WRITE
-    ENA=1'b1; ENB=1'b1;
-    ADDRA=10'd0; ADDRB=10'd0;
+    //Test Wirte
+    WEA=4'b1111; WEB=4'b1111;
+    ADDRA=10'd0; ADDRB=10'd4;
     DINA=32'hffffffff; DINB=32'hffffffff;
     #100
-    ENA=1'b0; ENB=1'b0;
+    WEA=4'b0000; WEB=4'b0000;
+    ADDRA=10'd0; ADDRB=10'd4;
+    DINA=32'd0; DINB=32'd0;
+    
     
     end
     
